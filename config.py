7f565061abbd2b110499193493c77ecedb1ba7fb
Adam <adam@gmail.com>
2025-05-19 20:32:33 -0300

    feat: add configuration classes for different environments

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+import random, string
+
+class Config:
+    CSRF_ENABLED = True
+    SECRET = 'flaks_a_z'
+    ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+    TEMPLATES_DIR = os.path.join(ROOT_DIR, 'templates')
+    APP = None
+
+class DevelopmentConfig(Config):
+    Testing = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 8000
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'
+
+class testingConfig(Config):
+    Testing = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 5000
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'
+
+class ProductionConfig(Config):
+    Testing = False
+    DEBUG = False
+    IP_HOST = 'localhost'
+    PORT_HOST = 8080
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'
+
+app_active = os.getenv('FlASK_ENV', 'development')
