085eb88b1c13d51f3488fff90f73ae532d2dfede
Adam <adam@gmail.com>
2025-05-19 20:50:48 -0300

    fix: add print statement for active configuration mode and ensure newline at end of file

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from config import app_active, app_config
 
 config = app_config[app_active]
 config.APP = create_app(app_active)
+print(f'Running in {app_active} mode')
 
 if __name__ == '__main__':
     config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
-    reload(sys)
+    reload(sys) 
