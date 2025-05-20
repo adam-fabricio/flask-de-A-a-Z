b9b4defa7f0d4a3df3279512ebf9528f2710ddb1
Adam <adam@gmail.com>
2025-05-19 21:42:45 -0300

    feat: finalizado até o capitulo 5 pagina 65

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import app_config, app_active
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,5 +17,37 @@ def create_app(config_name):
     
     @app.route('/')
     def index():
-        return "Hello, World!"
+        return "Meu primeiro run"
+    
+    @app.route('/login/')
+    def login():
+        return "Login"
+    
+    @app.route('/recovery-password/')
+    def recovery_password():
+        return "Recuperar senha"
+    
+    @app.route('/profile/<int:id>/action/<action>/')
+    def profile_action(id, action):
+        if action == 'action1':
+            return f"Perfil {id} - Ação 1"
+        elif action == 'action2':
+            return f"Perfil {id} - Ação 2"
+        elif action == 'action3':
+            return f"Perfil {id} - Ação 3"
+        
+    @app.route('/profile', methods=['POST'])
+    def create_profile():
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return f"Usuário {username} criado com sucesso!"
+    
+    @app.route('/profile/<int:id>', methods=['PUT'])
+    def update_profile(id):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return f"Usuário {id} atualizado com sucesso!"  
+    
     return app
